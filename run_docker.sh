24d99aca55582ffbc80ec4366b60a77869ea6320
Ubuntu <ubuntu@ip-172-31-17-220.us-west-2.compute.internal>
2020-06-14 04:16:22 +0000

    Primary update

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 
 docker build --tag susmithasusmi13/kubernetes-clusters-demo:devopsprojectcapstone .
 
-docker login
+#docker login
 
 docker push susmithasusmi13/kubernetes-clusters-demo:devopsprojectcapstone
